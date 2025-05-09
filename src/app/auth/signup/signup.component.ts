12eb699d7cece0959f0a8b189b6f091f1b4c7e73
gmartpad <contato.gabrielmartinspadoin@gmail.com>
2025-05-09 07:28:02 -0300

    [NG] CREATING_MULTI_INPUT_VALIDATORS_FORM_GROUP_VALIDATORS - feat: creating a factory function that creates a validator function for formGroups to check if 2 control values are equal to each other

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -12,6 +12,20 @@ function emailIsUnique(control: AbstractControl) {
   return of({ notUnique: true });
 }
 
+function equalValues(controlName1: string, controlName2: string) {
+
+  return (control: AbstractControl) => {
+    const val1 = control.get(controlName1)?.value;
+    const val2 = control.get(controlName2)?.value;
+  
+    if (val1 === val2) {
+      return null;
+    }
+    return { valuesNotEqual: true };
+  }
+  
+}
+
 @Component({
   selector: 'app-signup',
   standalone: true,
@@ -32,6 +46,8 @@ export class SignupComponent {
       confirmPassword: new FormControl('', {
         validators: [Validators.required, Validators.minLength(6)],
       }),
+    }, {
+      validators: [equalValues('password', 'confirmPassword')]
     }),
     firstName: new FormControl('', { validators: [Validators.required] }),
     lastName: new FormControl('', { validators: [Validators.required] }),
